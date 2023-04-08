b77a68d477305785cdf39f8c86c96568344840a9
Shekhar Dubey <dubeyshekhar1997@gmail.com>
2023-04-09 03:46:15 +0530

    Added changes for Personal Portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import PortFolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import SocialLink from "./components/SocialLink";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <>
+   <NavBar/>
+   <Home />
+   <About />
+   <PortFolio />
+   <Experience />
+   <Contact />
+   <SocialLink />
+   </>
+);
+};
 
 export default App;
